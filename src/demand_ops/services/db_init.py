187c31217e2d49000d6659338129513acb5e4bd7
unknown <apiyush30@gmail.com>
2026-01-28 23:51:43 -0500

    Addinf metrics functions

--- src/demand_ops/services/db_init.py
+++ src/demand_ops/services/db_init.py
@@ -1,6 +1,8 @@
 from demand_ops.db.base import Base
 from demand_ops.db.session import ENGINE
 import demand_ops.db.models  # noqa: F401  (ensures models are imported)
+import demand_ops.db.metrics_models  # noqa: F401
+
 
 def init_db():
     Base.metadata.create_all(bind=ENGINE)
